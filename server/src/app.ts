2e304a03bbb08078db0ece921715bcd636f4bd32
Henry <henrysavit@gmail.com>
2024-04-30 00:52:05 -0400

    server code

---
+++ server/src/app.ts
@@ -0,0 +1,33 @@
+
+import express from 'express';
+import cors from 'cors';
+import axios from 'axios';
+import { WeatherData } from './types';
+
+const app = express();
+
+app.use(cors()); 
+
+const API_KEY = process.env.API_KEY || "3d8fcb5ce8a249b38d8150736242904"
+const BASE_URL = "http://api.weatherapi.com/v1"
+
+app.listen(3001, () => {
+  console.log('Server listening on port 3001');
+});
+
+
+app.get('/api/weather', async (req, res) => {
+  const city = req.query.city as string; 
+
+  if (!city) {
+    return res.status(400).json({ error: 'Please provide a city name' });
+  }
+
+  try {
+    const response = await axios.get<WeatherData>(`${BASE_URL}/current.json?key=${API_KEY}&q=${city}`); // Adjust API call if needed
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Error fetching weather data' });
+  }
+});
